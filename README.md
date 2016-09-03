10ad27aeb3f6d8c85856ab3f4c60a55caa5cca59
Zikeji <caleb@blankemeyer.us>
2016-09-03 06:43:26 -0400

    initial commit, working

---
+++ README.md
@@ -0,0 +1,20 @@
+# discord-feed-bot
+
+### Installation
+
+ - `git clone https://git.zynados.com/homelab/discord-feed-bot`
+ - `cd discord-feed-bot`
+ - `npm install`
+ - Set the following environment variables. Preferably just make a .env file with them (`VARIABLE=VALUE`)
+    - `DISCORD_TOKEN` // the bot API token you've created
+    - `DISCORD_SERVERID` // the server ID of the server you want to use this one (Server Settings -> Widget -> "SERVER ID")
+    - `DISCORD_CHANNEL` // the channel you want feed updates to be posted to
+    - `SUBREDDIT` // the subreddit you want to notify on
+    - `LOGGING_LEVEL` // what you want to log, can be either: debug, info, warn, error
+- Now start the bot using `npm start`
+
+#### Installing as a service (Linux)
+
+If you would like to run the bot as a service, I've included a systemd example file (`discordfeedbot.service.example`). Configure it and install it as `discordfeedbot.service` to `/etc/systemd/system`.
+
+You can now run `systemctl start discordfeedbot.service` to start the bot in the background. To enable it on bootup, simply `systemctl enable discordfeedbot.service`. To review application logs, simply `journalctl -t discordfeedbot`.
---
+++ app.js
@@ -0,0 +1,70 @@
+'use strict';
+
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const request = require('request');
+const logger = require('./logger');
+
+const bot = new Discord.Client();
+bot.loginWithToken(process.env.DISCORD_TOKEN);
+logger.info('Initialized');
+
+bot.on('ready', () => {
+  bot.setStatus('online', `Spamming F5 on /r/${process.env.SUBREDDIT}`);
+  let lastTimestamp = Math.floor(Date.now() / 1000);
+  let Channel = null;
+
+  for (const server of bot.servers) {
+    if (server.id === process.env.DISCORD_SERVERID) {
+      for (const channel of server.channels) {
+        if (channel.name === process.env.DISCORD_CHANNEL) {
+          Channel = channel;
+        }
+      }
+    }
+  }
+
+  if (!Channel) {
+    logger.error('A matching channel could not be found. Please check your DISCORD_SERVERID and DISCORD_CHANNEL environment variables.');
+    process.exit(1);
+  } else {
+    logger.info('Ready');
+  }
+
+  const subredditUrl = `https://www.reddit.com/r/${process.env.SUBREDDIT}/new.json?limit=10`;
+
+  setInterval(() => {
+    request({
+      url: subredditUrl,
+      json: true,
+    }, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        logger.debug('Request succeeded, lastTimestamp = ', lastTimestamp);
+        for (const post of body.data.children.reverse()) {
+          if (lastTimestamp <= post.data.created_utc) {
+            lastTimestamp = post.data.created_utc;
+            let formattedPost = null;
+            if (post.data.is_self) {
+              formattedPost = `New text post in */r/${process.env.SUBREDDIT}*\n\n`;
+              formattedPost += `**${post.data.title}**\n`;
+              formattedPost += `${post.data.selftext.length > 253 ? post.data.selftext.slice(253).concat('...') : post.data.selftext}\n`;
+              formattedPost += `https://redd.it/${post.data.id}`;
+            } else {
+              formattedPost = `New link post in */r/${process.env.SUBREDDIT}*\n\n`;
+              formattedPost += `**${post.data.title}**\n`;
+              formattedPost += `${post.data.url}\n`;
+              formattedPost += `https://redd.it/${post.data.id}`;
+            }
+            bot.sendMessage(Channel, formattedPost);
+            logger.info(`Sent message for new post https://redd.it/${post.data.id}`);
+          }
+        }
+        ++lastTimestamp; // so it's one above so it won't match them from previous queries
+      } else {
+        logger.warn('Request failed - reddit could be down or subreddit doesn\'t exist. Will continue.');
+        logger.debug(response, body);
+      }
+    });
+  }, 30000);
+});
